4ace6ffac62e008104cfc5ce72b87737770c1323
annelaframboise <75187728+annelaframboise@users.noreply.github.com>
2021-06-05 11:06:04 -0700

    Add files via upload

---
+++ design/data_table.js
@@ -0,0 +1,134 @@
+// Get data and display
+d3.csv("/data/winequality-TOTAL.csv").then(function (data) {
+  console.log(data);
+  //   Set wine type dropdown menu
+  const winetypeDropdownMenuSelection = d3.selectAll("#selWineType");
+  const winetypeDropdownMenu = winetypeDropdownMenuSelection.node();
+  const unique_winetype = new Set(data.map((entry) => entry.type));
+  // Add a opt for both wine types
+  const opt_all_winetype = document.createElement("option");
+  opt_all_winetype.value = "";
+  opt_all_winetype.innerHTML = "[All Types]";
+  winetypeDropdownMenu.appendChild(opt_all_winetype);
+  // Add opts for both wine types to the dropdown menu
+  [...unique_winetype].sort().forEach((type) => {
+    const opt = document.createElement("option");
+    opt.value = type;
+    opt.innerHTML = type;
+    winetypeDropdownMenu.appendChild(opt);
+  });
+  console.log(winetypeDropdownMenu);
+
+  // Set Quality dropdown menu
+  const QualityDropdownMenuSelection = d3.selectAll("#selQuality");
+  const QualityDropdownMenu = QualityDropdownMenuSelection.node();
+  const unique_quality = new Set(data.map((entry) => entry.quality));
+  // Add opt for no quality
+  const opt_no_quality = document.createElement("option");
+  opt_no_quality.value = "";
+  opt_no_quality.innerHTML = "[All Quality]";
+  QualityDropdownMenu.appendChild(opt_no_quality);
+  // Add opts for all quality
+  [...unique_quality].sort().forEach((quality) => {
+    const opt_element = document.createElement("option");
+    opt_element.value = quality;
+    opt_element.innerHTML = quality;
+    QualityDropdownMenu.appendChild(opt_element);
+  });
+
+  // Append Table heading and Column names
+  const table = document.querySelector("table");
+  const thead = table.createTHead();
+  const tbody = document.createElement("tbody");
+  const row = thead.insertRow();
+  for (const key in data[0]) {
+    const th = document.createElement("th");
+    const text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+  function make_table(data) {
+    // Add table contents
+
+    // Function to sort data by quality score and type
+    function byQualityScoreandType(a, b) {
+      if (a.quality < b.quality) return -1;
+      if (a.quality === b.quality) {
+        if (a.type < b.type) return -1;
+        if (a.type === b.type) return 0;
+      }
+      return 1;
+    }
+    data.sort(byQualityScoreandType).forEach((entry) => {
+      let row = tbody.insertRow();
+      Object.values(entry).forEach((value) => {
+        let cell = row.insertCell();
+        const text = document.createTextNode(value);
+        cell.appendChild(text);
+      });
+    });
+    table.appendChild(tbody);
+  }
+  make_table(data);
+
+
+
+  // THIS IS WHERE YOU LEFT OFF
+  
+  let lastSelectedQuality = QualityDropdownMenu.value;
+  function updateTable(event) {
+    // Prevent the page from refreshing
+    event.preventDefault();
+    // Clear any input
+    while (tbody.firstChild) tbody.removeChild(tbody.lastChild);
+    
+    // Retrive quality selected
+    const quality_selected = QualityDropdownMenu.value;
+    if (lastSelectedQuality !== quality_selected) {
+      lastSelectedQuality = quality_selected;
+      const hasQuality = lastSelectedQuality !== "";
+      winetypeDropdownMenu.innerHTML = "";
+      const qualityWineType = new Set(
+        data.flatMap((entry) =>
+          !hasQuality || lastSelectedQuality === entry.quality ? entry.type : []
+        )
+      );
+      winetypeDropdownMenu.appendChild(opt_all_winetype);
+      [...qualityWineType].sort().forEach((country) => {
+        const opt = document.createElement("option");
+        opt.value = country;
+        opt.innerHTML = country;
+        winetypeDropdownMenu.appendChild(opt);
+      });
+    }
+
+    // Retrive type selected
+    const winetype_selected = winetypeDropdownMenu.value;
+    
+    if (winetype_selected === "") {
+      if (quality_selected === "") {
+        make_table(data);
+      } else {
+        // Filter data based on selected region
+        const quality_filtered_display_data = data.filter(
+          (entry) => entry.quality === quality_selected
+        );
+
+        make_table(quality_filtered_display_data);
+      }
+    } else {
+      // Filter data based on selected country
+      const winetype_filtered_display_data = data.filter(
+        (entry) => entry.type === winetype_selected
+      );
+
+      make_table(winetype_filtered_display_data);
+    }
+  }
+  // Create event handler, set on chage
+  winetypeDropdownMenuSelection.on("input", updateTable);
+
+  // move this to the new function***********
+
+  QualityDropdownMenuSelection.on("input", updateTable);
+});
